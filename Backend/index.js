c3d933deae9536c0d7e741d24456db75fdb0ec01
wahyukurniaa <wahyukurnia321123@gmail.com>
2022-02-01 22:58:01 +0700

    cords added()

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import db from "./config/database.js";
 import productRoutes from './routes/index.js';
+import cors from 'cors';
 
 const app = express();
 
@@ -10,7 +11,7 @@ try{
 }catch(error){
     console.log("Database Error", error);
 }
-
+app.use(cors());
 app.use(express.json());
 app.use('/products',productRoutes);
 
